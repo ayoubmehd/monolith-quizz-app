afce9da0bbfaac10cfd7367945fb2c000bf3633e
ayoubmehd <safi-_-gio-raja@live.fr>
2021-12-14 10:07:27 +0100

    chore: remove db recreation on server restart

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const { sequelize } = require("./models");
 
 sequelize.authenticate()
   .then(async () => {
-    await sequelize.sync({ force: true });
+    // await sequelize.sync({ force: true });
     console.log('Connection has been established successfully.')
   })
   .catch(error => console.error('Unable to connect to the database:', error));

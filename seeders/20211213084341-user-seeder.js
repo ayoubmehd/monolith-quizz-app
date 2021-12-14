e6cbbaa761e2d9c5b10fbfffea4837cfaaa07b9f
ayoubmehd <safi-_-gio-raja@live.fr>
2021-12-14 10:11:59 +0100

    test: user seed

---
+++ seeders/20211213084341-user-seeder.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const faker = require("faker");
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+    await queryInterface.bulkInsert('Users', [{
+      firstName: faker.name.firstName(),
+      lastName: faker.name.lastName(),
+      email: faker.internet.email(),
+      password: "123456789",
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }], {});
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};

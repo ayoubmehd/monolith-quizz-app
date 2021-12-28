40a1fb2515d0d7a6955d6fb1c63f590ffd52c56f
ayoubmehd <safi-_-gio-raja@live.fr>
2021-12-28 13:29:32 +0100

    test(seed): difficulty level

--- seeders/20211222085550-question-seeder.js
+++ seeders/20211222085550-question-seeder.js
@@ -2,7 +2,7 @@
 
 
 const faker = require("faker");
-const answer = require("../repository/global")("Answer");
+const diffLevl = require("../repository/global")("DifficultyLevel");
 const question = require("../repository/global")("Question");
 
 module.exports = {
@@ -17,40 +17,61 @@ module.exports = {
      * }], {});
     */
 
+    await queryInterface.bulkInsert("DifficultyLevels", [
+      {
+        label: "Easy",
+        description: "Easy",
+        numOfPointsMin: 5,
+        numOfPointsMax: 10,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        label: "Medium",
+        description: "Medium",
+        numOfPointsMin: 11,
+        numOfPointsMax: 15,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        label: "Hard",
+        description: "Hard",
+        numOfPointsMin: 16,
+        numOfPointsMax: 20,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+    ]);
+
     const elCount = 20;
+    const [diffLevel, diffLevelRes] = await diffLevl.findAll();
 
     const qArray = [];
     for (let i = 0; i < elCount; i++) {
       qArray.push({
         question: faker.lorem.words(5),
-        createdAt: new Date(),
-        updatedAt: new Date()
-      })
-    }
-    const aArray = [];
-    for (let i = 0; i < elCount; i++) {
-      aArray.push({
-        text: faker.lorem.words(3),
-        createdAt: new Date(),
-        updatedAt: new Date()
-      })
-    }
-    const aqArray = [];
-    const [asnwerErr, answerRes] = await answer.findAll();
-    const [questionErr, questionRes] = await question.findAll();
-    for (let i = 0; i < elCount; i++) {
-      aqArray.push({
-        isCorrect: faker.random.arrayElement([true, false]),
-        AnswerId: faker.random.arrayElement(answerRes).id,
-        QuestionId: faker.random.arrayElement(questionRes).id,
+        DifficultyLevelId: faker.random.arrayElement(diffLevelRes).id,
         createdAt: new Date(),
         updatedAt: new Date()
       })
     }
 
-    await queryInterface.bulkInsert('Answers', aArray);
     await queryInterface.bulkInsert('Questions', qArray);
-    await queryInterface.bulkInsert('AnswerQuestions', aqArray);
+
+    const aArray = [];
+    const [questionErr, questionRes] = await question.findAll();
+    for (let i = 0; i < elCount; i++) {
+      aArray.push({
+        isCorrect: faker.random.arrayElement([true, false]),
+        QuestionId: faker.random.arrayElement(questionRes).id,
+        text: faker.lorem.words(3),
+        createdAt: new Date(),
+        updatedAt: new Date()
+      })
+    }
+    await queryInterface.bulkInsert('Answers', aArray);
+
 
   },
 
